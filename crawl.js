98a8dab529481e2ba72f718967f1c914eb6f9059
KADTLAKSHAN <kadtlakshan@gmail.com>
2025-08-21 19:24:55 +0530

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlbody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlbody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      // relative
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log(`error with relative url: ${error.meesage}`);
+      }
+    } else {
+      // absolute
+      try {
+        const urlObj = new URL(`${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log(`error with absolute url: ${error.meesage}`);
+      }
+    }
+  }
+  return urls;
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -9,4 +37,5 @@ function normalizeURL(urlString) {
 
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
